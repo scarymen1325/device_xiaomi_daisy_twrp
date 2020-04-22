d976ebec43f9bd717abb56b99114911a098c6f64
scarymen1325 <58949658+scarymen1325@users.noreply.github.com>
2020-04-22 03:26:07 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-ifneq ($(filter daisys,$(TARGET_DEVICE)),)
+ifneq ($(filter daisy,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
